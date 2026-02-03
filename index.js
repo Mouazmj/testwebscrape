7ab5bb78bd460654b16de3cecd4de9078ddbf841
Mouaz Jamal <mj225ih@student.lnu.se>
2026-02-03 21:14:32 +0100

    Import the packages and set up the basics for the func and set url to scrape to a varibel

---
+++ index.js
@@ -0,0 +1,14 @@
+import cheerio from cheerio;
+import axios from axios;
+
+const url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+
+async function fetchBookData () {
+    try {
+
+    } catch (error) {
+        console.error('Error fetching data:', error)
+    }
+}
+
+fetchBookData()
