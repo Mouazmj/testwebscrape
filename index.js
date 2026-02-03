11c7035a1d5c514c20fb49b0ab813cab3a54f9b4
Mouaz Jamal <mj225ih@student.lnu.se>
2026-02-03 21:38:14 +0100

    Get the url with axios and load it with cheerio so I can make a varibel to collect all the h1 headers and make it to a text and console it

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index
 
 async function fetchBookData () {
     try {
+        const response = await axios.get(url)
+        const $ = cheerio.load(response.data)
+        const genre = $('h1').text()
 
+        console.log('Genre: ', genre)
     } catch (error) {
         console.error('Error fetching data:', error)
     }
